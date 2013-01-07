89af0e6ea6c371ff64ef0a46b3987dac09d347dc
Michael Spear <git@bsd4breakfast.us>
2013-01-07 12:17:17 -0600

    a few todo changes

--- configure/EMAxSTATIC.php
+++ configure/EMAxSTATIC.php
@@ -9,7 +9,7 @@ class EMAxSTATIC
 	public static $USE_GOOGLE_CAL = FALSE;
 	public static $USE_IMAP_EMAIL = FALSE;
 	public static $USE_SSL = FALSE;
-	public static $PATH_ZEND = '../ZendGdata-1.10.5/library';
+	public static $PATH_ZEND = '../../ZendGdata-1.10.5/library';
 	public static $PATH_JQUERY = '//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js';
 	public static $PATH_JQUERY_UI = '//ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/jquery-ui.min.js';
 	public static $PATH_JQUERY_UI_CSS = '//ajax.googleapis.com/ajax/libs/jqueryui/1.9.0/themes/base/jquery-ui.css';
